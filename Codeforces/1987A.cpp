b4cb608ea521f3867198257b072d5a71bd4ffbe0
ZIHAN <zihanforall@gmail.com>
2024-07-11 11:51:02 +0600

    1987A

---
+++ Codeforces/1987A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t,n,k;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        cin >> n >> k;
+        cout << (n-1)*k + 1 << endl;
+    }
+
+    
+    return 0;
+}
